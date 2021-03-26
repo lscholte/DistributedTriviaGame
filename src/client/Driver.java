a058ab7086a5d8b8d9540948dcc3894b3f61c6b8
Liam Scholte <lscholte@uvic.ca>
2021-03-25 23:04:30 -0700

    Removes unused import

--- src/client/Driver.java
+++ src/client/Driver.java
@@ -3,8 +3,6 @@ package client;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
-import utilities.Logger;
-
 public class Driver {
     
   public static void main(String[] args) throws UnknownHostException, IOException {    
