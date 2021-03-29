2ace4032b0999800ac3b47127c808041574bc462
Liam Scholte <lscholte@uvic.ca>
2021-03-28 17:08:59 -0700

    Fixes indentation to 4 spaces per tab for consistency

--- src/game/Server.java
+++ src/game/Server.java
@@ -12,42 +12,40 @@ import protobuf.generated.LobbyServiceMessages.JoinLobbyRequest;
 import protobuf.generated.LobbyServiceMessages.JoinLobbyResponse;
 
 public class Server {
-  public static final int PORT = 7766;
-  
-  private io.grpc.Server grpcServer;
-  
-  public Server() {
-    grpcServer = ServerBuilder
-        .forPort(PORT)
-        .addService(new LobbyService())
-        .build();
-    
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      try {
-        grpcServer.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-      }
-      catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    }));
-  }
+    public static final int PORT = 7766;
 
-  public void start() throws IOException, InterruptedException {    
-    grpcServer.start();
-    grpcServer.awaitTermination();
-  }
-      
-  private class LobbyService extends LobbyServiceImplBase {
-    
-    @Override
-    public void createLobby(CreateLobbyRequest request, StreamObserver<CreateLobbyResponse> responseObserver) {
-      //TODO: Create a lobby, assign a UUID and return the UUID back to client
+    private io.grpc.Server grpcServer;
+
+    public Server() {
+        grpcServer = ServerBuilder.forPort(PORT).addService(new LobbyService()).build();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                grpcServer.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
     }
-    
-    @Override
-    public void joinLobby(JoinLobbyRequest request, StreamObserver<JoinLobbyResponse> responseObserver) {
-      //TODO: add the player to the lobby.
-      //Setup a channel with the player to send them questions when the game starts
+
+    public void start() throws IOException, InterruptedException {
+        grpcServer.start();
+        grpcServer.awaitTermination();
     }
-  }
+
+    private class LobbyService extends LobbyServiceImplBase {
+
+        @Override
+        public void createLobby(CreateLobbyRequest request,
+                StreamObserver<CreateLobbyResponse> responseObserver) {
+            // TODO: Create a lobby, assign a UUID and return the UUID back to client
+        }
+
+        @Override
+        public void joinLobby(JoinLobbyRequest request,
+                StreamObserver<JoinLobbyResponse> responseObserver) {
+            // TODO: add the player to the lobby.
+            // Setup a channel with the player to send them questions when the game starts
+        }
+    }
 }
